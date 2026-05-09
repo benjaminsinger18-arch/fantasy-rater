f02b3febc403c524ef3807aa35bd3086ce351a78
Benjamin Singer <bsinger2@babson.edu>
2026-05-09 13:57:27 -0400

    fix: service worker skipWaiting + clientsClaim for instant updates on deploy

--- fantasy-rater/client/src/sw.js
+++ fantasy-rater/client/src/sw.js
@@ -2,6 +2,9 @@ import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
+self.skipWaiting();
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
 // vite-plugin-pwa replaces self.__WB_MANIFEST with the list of built assets at build time
 precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
